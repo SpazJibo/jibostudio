70355b8f5fcf983e83e1ecec7b5477ccfd00e5fd
Curtis Allen Gibeaut Jr <curtjibo@gmail.com>
2022-01-12 14:35:27 -0600

    Added Footer and some customer colors for the theme

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,20 @@
 <script>
 	import '../app.css';
 </script>
-<header>
-    <h1 class="text-4xl">Jibo Studio</h1>
+
+<header class="bg-gray-300 text-jibo-red p-5">
+    <div class="container">
+        <h1 class="text-3xl font-bold ">
+            Jibo Stud<span class="text-jibo-gray">io</span>
+        </h1>
+    </div>
 </header>
-<slot />
+
+<div class="m-5">
+    <slot />
+</div>
+
+<div class="text-center absolute inset-x-0 bottom-0 h-16 bg-gray-300 
+    text-jibo-red p-5">
+    Copyright &copy; 2022 Jibo Studio LLC 
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+        extend: {
+            colors: {
+                'jibo-red': '#740024',
+                'jibo-gray': '#4D4D4D',
+            }
+        }
 	},
 
 	plugins: []
